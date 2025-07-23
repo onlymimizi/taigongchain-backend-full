6d1f137f8d475f3e5ce69264961de27d46e1dc7a
onlymimizi <2214003943@qq.com>
2025-07-23 18:47:59 +0800

    Update index.ts

--- taigongchain-backend-full/src/index.ts
+++ taigongchain-backend-full/src/index.ts
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Taigongchain Backend is running!');
+});
 app.use('/api', routes);
 
 app.listen(PORT, () => {
